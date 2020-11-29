5ac38994203be305db33f44579cf42f8af618dee
jeswr <jesse.wright@anu.edu.au>
2020-11-30 10:18:40 +1100

    WIP: Add definitinons for IActorRdfUpdate

--- packages/bus-rdf-update-quads/lib/ActorRdfUpdateQuads.ts
+++ packages/bus-rdf-update-quads/lib/ActorRdfUpdateQuads.ts
@@ -1,12 +1,14 @@
-import { Actor, IAction, IActorArgs, IActorOutput, IActorTest } from '@comunica/core';
+import type { QuadStream } from '@comunica/bus-query-operation';
+import type { ActionContext, IAction, IActorArgs, IActorOutput, IActorTest } from '@comunica/core';
+import { Actor } from '@comunica/core';
 
 /**
  * A comunica actor for rdf-update-quads events.
  *
  * Actor types:
- * * Input:  IActionRdfUpdateQuads:      TODO: fill in.
+ * * Input:  IActionRdfUpdateQuads:      Primary quad stream and streams of quads to be inserted and deleted.
  * * Test:   <none>
- * * Output: IActorRdfUpdateQuadsOutput: TODO: fill in.
+ * * Output: IActorRdfUpdateQuadsOutput: Streams of quads that were inserted and deleted.
  *
  * @see IActionRdfUpdateQuads
  * @see IActorRdfUpdateQuadsOutput
@@ -18,9 +20,51 @@ export abstract class ActorRdfUpdateQuads extends Actor<IActionRdfUpdateQuads, I
 }
 
 export interface IActionRdfUpdateQuads extends IAction {
-
+  /**
+   * The stream of quads that are to be updated
+   */
+  quads: QuadStream;
+  /**
+   * An optional field indicating if the given quad stream originates from a triple-based serialization,
+   * in which everything is serialized in the default graph.
+   * If falsy, the quad stream contain actual quads, otherwise they should be interpreted as triples.
+   */
+  triples?: boolean;
+  /**
+   * The stream of quads to be inserted.
+   * Undefined if the if no quads are to be inserted.
+   */
+  quadStreamInserted?: QuadStream;
+  /**
+   * The stream of quads to be deleted.
+   * Undefined if the if no quads are to be deleted.
+   */
+  quadStreamDeleted?: QuadStream;
+  /**
+   * A convenience constructor for {@link ActionContext} based on a given hash.
+   * @param {{[p: string]: any}} hash A hash that maps keys to values.
+   * @return {ActionContext} The immutable action context from the hash.
+   * @constructor
+  */
+  context?: ActionContext;
 }
 
 export interface IActorRdfUpdateQuadsOutput extends IActorOutput {
-
+  /**
+   * The stream of quads that were inserted.
+   * Undefined if the operation did not have to insert anything.
+   */
+  quadStreamInserted?: QuadStream;
+  /**
+   * The stream of quads that were deleted.
+   * Undefined if the operation did not have to delete anything.
+   */
+  quadStreamDeleted?: QuadStream;
+  /**
+   * A convenience constructor for {@link ActionContext} based on a given hash.
+   * @param {{[p: string]: any}} hash A hash that maps keys to values.
+   * @return {ActionContext} The immutable action context from the hash.
+   * @constructor
+  */
+  context?: ActionContext;
 }
