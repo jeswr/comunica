afde6663e5a84d7897228f417acc29bcea9e9d2c
jeswr <jesse.wright@anu.edu.au>
2020-12-05 16:41:59 +1100

    WIP: Create rdf-combine-quads & filling in other buses/actors

--- packages/actor-query-operation-update-compositeupdate/lib/ActorQueryOperationUpdateCompositeUpdate.ts
+++ packages/actor-query-operation-update-compositeupdate/lib/ActorQueryOperationUpdateCompositeUpdate.ts
@@ -1,22 +1,53 @@
-import type { IActorQueryOperationOutput,
+import {
+  ActorQueryOperationTyped,
+  IActorQueryOperationOutput,
   IActorQueryOperationOutputUpdate,
   IActorQueryOperationTypedMediatedArgs,
-  QuadStream } from '@comunica/bus-query-operation';
+  QuadStream,
+} from '@comunica/bus-query-operation';
 import {
   ActorQueryOperation, ActorQueryOperationTypedMediated,
 } from '@comunica/bus-query-operation';
-import type { ActionContext, IActorTest } from '@comunica/core';
+import type {
+  ActorRdfCombineQuads,
+  IActionRdfCombineQuads,
+  IActorRdfCombineQuadsOutput
+} from '@comunica/bus-rdf-combine-quads';
+import type {
+  ActorRdfJoinQuads,
+  IActionRdfJoinQuads,
+  IActorRdfJoinQuadsOutput
+} from '@comunica/bus-rdf-join-quads';
+import type { ActionContext, IActorArgs, IActorTest, MediatedActor } from '@comunica/core';
+import { AsyncIterator } from 'asynciterator';
+import type * as RDF from 'rdf-js';
 import type { Algebra } from 'sparqlalgebrajs';
+// TODO: FIX NAMING
+export abstract class ActorRDFCombineOperationTypedMediated extends ActorQueryOperationTypedMediated<Algebra.CompositeUpdate> implements IMediatedCombineQuads {
+  public readonly mediatorJoinQuads: MediatedActor<IActionRdfJoinQuads, IActorTest, IActorRdfJoinQuadsOutput>;
+  public readonly mediatorCombineQuads: MediatedActor<IActionRdfCombineQuads, IActorTest, IActorRdfCombineQuadsOutput>;
+
+  public constructor(args: IActorQueryOperationTypedMediatedArgs, operationName: string) {
+    super(args, operationName);
+  }
+}
+
+interface IMediatedCombineQuads extends ActorQueryOperationTypedMediated<Algebra.CompositeUpdate> {
+  // TODO: CHECK IActorTest
+  mediatorCombineQuads: MediatedActor<IActionRdfCombineQuads, IActorTest, IActorRdfCombineQuadsOutput>;
+}
 
 /**
  * A comunica Update CompositeUpdate Query Operation Actor.
  */
 export class ActorQueryOperationUpdateCompositeUpdate
-  extends ActorQueryOperationTypedMediated<Algebra.CompositeUpdate> {
+  extends ActorRDFCombineOperationTypedMediated implements IMediatedCombineQuads {
+  mediatorCombineQuads: MediatedActor<IActionRdfCombineQuads, IActorTest, IActorRdfCombineQuadsOutput>;
+
   public constructor(args: IActorQueryOperationTypedMediatedArgs) {
     super(args, 'compositeupdate');
   }
-
+    
   public async testOperation(pattern: Algebra.CompositeUpdate, context: ActionContext): Promise<IActorTest> {
     return true;
   }
@@ -29,12 +60,22 @@ export class ActorQueryOperationUpdateCompositeUpdate
 
     const updateResultsSafe: IActorQueryOperationOutputUpdate[] = updateResults.map(ActorQueryOperation.getSafeUpdate);
 
-    const quadStreamInserted: QuadStream[] = updateResultsSafe.flatMap(update => update.quadStreamInserted);
+    const quadStreamsInserted: QuadStream[] = updateResultsSafe.map(
+      update => update.quadStreamInserted
+    );
+    const quadStreamsDeleted: QuadStream[] = updateResultsSafe.map(
+      update => update.quadStreamDeleted
+    );
 
+    // TOOO: FIX Cancelleation of update and insert streams - probably just hook directly into update delete
     return {
       type: 'update',
-      quadStreamInserted, // TODO: join input streams
-      quadStreamDeleted, // TODO: join input streams
+      quadStreamInserted: (await this.mediatorJoinQuads.mediate({
+        quadStreams: quadStreamsInserted,
+      })).quads,
+      quadStreamDeleted: (await this.mediatorJoinQuads.mediate({
+        quadStreams: quadStreamsDeleted,
+      })).quads,
     };
   }
 }
